312b1c82a334811f7394fa2770cbffffc2eb79c8
Lucas Cavalare <lucas.cavalare@workday.com>
2022-05-06 17:26:11 +0100

    CI day3

--- beerlog/api.py
+++ beerlog/api.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI  # ASGI
+from fastapi import FastAPI, Response, status  # ASGI
 
 # Needs 'poetry add uvicorn'
 # uvicorn beerlog.api:api
 from beerlog.core import get_beers_from_database
-from typing import List
+from typing import List, Optional
 
 # from beerlog.models import Beer
 from beerlog.serializers import BeerOut, BeerIn
@@ -11,18 +11,18 @@ from beerlog.database import get_session
 from beerlog.models import Beer
 
 # Create and name API
-api = FastAPI(title="Beerlog")
+api = FastAPI(title="beerlog")
 
 
 # Decorator
 @api.get("/beers", response_model=List[BeerOut])
 # Async to distrubute processing
-async def list_beers():
-    beers = get_beers_from_database()
+async def list_beers(style: Optional[str] = None):
+    beers = get_beers_from_database(style)
     return beers
 
 
-@api.post("/beers", response_model=List[BeerOut])
+@api.post("/beers", response_model=BeerOut)
 async def add_beer(beer_in: BeerIn, response: Response):
     beer = Beer(**beer_in.dict())
     with get_session() as session:
