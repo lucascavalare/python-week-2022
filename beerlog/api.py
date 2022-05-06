441dfa1f05e5e583e08d9650d447e9ce8371a7c9
Lucas Cavalare <lucas.cavalare@workday.com>
2022-05-06 17:20:10 +0100

    CI day3

--- beerlog/api.py
+++ beerlog/api.py
@@ -23,9 +23,13 @@ async def list_beers():
 
 
 @api.post("/beers", response_model=List[BeerOut])
-async def add_beer(beer_in: BeerIn):
+async def add_beer(beer_in: BeerIn, response: Response):
     beer = Beer(**beer_in.dict())
     with get_session() as session:
         session.add(beer)
         session.commit()
         session.refresh(beer)
+
+
+    response.status_code = status.HTTP_201_CREATED
+    return beer
